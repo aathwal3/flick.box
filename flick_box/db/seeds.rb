5a1d593999a93d12541903337c7198c96885893c
aathwal3 <aathwal3@gmail.com>
2015-11-16 13:53:38 -0800

    add seed file

--- flick_box/db/seeds.rb
+++ flick_box/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([{name: "test1", email: "test1@email.com", password: "test1"}])
+videos = Video.create([{title: "The Notebook", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "available"},
+ {title: "Forrest Gump", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "available"}, 
+ {title: "Fight Club", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "rented"},
+ {title: "The Lion King", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "rented"},
+ {title: "The Dark Knight", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "rented"},
+ {title: "Transformers", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "rented"},
+ {title: "Avatar", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1, status: "damaged"},
+ {title: "Inception", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1,  status: "damaged"},
+ {title: "Bridesmaids", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1,  status: "damaged"},
+ {title: "The Matrix", description: Faker::Lorem.paragraph, release_date: Faker::Date.backward(1000), user_id: 1,  status: "damaged"}])
