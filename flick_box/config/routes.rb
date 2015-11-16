bffe300761a4f65f23259e8dfe1aab672798856e
aathwal3 <aathwal3@gmail.com>
2015-11-16 13:50:35 -0800

    add bcrypt authentication to user login

--- flick_box/config/routes.rb
+++ flick_box/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :users
+  root to: 'users#index'
+
+  get'/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+  get '/search' => 'videos#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
